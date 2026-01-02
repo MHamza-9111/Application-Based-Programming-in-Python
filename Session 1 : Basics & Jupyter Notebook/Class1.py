bf135cd0094d0c87c6b44ab2bff2e8cde11bc250
Muhammad_Hamza <mughalhamza2015@gmail.com>
2026-01-02 17:05:51 +0500

    Add files via upload

---
+++ Session 1 : Basics & Jupyter Notebook/Class1.py
@@ -0,0 +1,23 @@
+# Session 1 Print
+
+print("Hello World!")   # This line prints "Hello World!" to the console
+
+# Session 2 Comments
+
+# This is a single-line comment explaining that the next line of code prints a message
+
+# Session 3 Arithmetic Operations
+print(2 + 3)  # Addition: prints the result of 2 + 3
+print (2 - 3) # Subtraction: prints the result of 2 - 3
+print (2 * 3) # Multiplication: prints the result of 2 * 3
+print (25 / 5) # Division: prints the result of 25 / 5
+print (25 // 5) # Floor Division: prints the result of 25 // 5
+
+# Session 4 Logical Operations
+print (2 > 3)  # Greater than: prints False since 2 is not greater than 3
+print (2 < 3)  # Less than: prints True since 2 is less than 3
+print (2 == 2) # Equality: prints True since 2 is equal to 2
+print(2 != 3) # Not equal: prints True since 2 is not equal to 3
+print (2 >= 3) # Greater than or equal to: prints False since 2 is not greater than or equal to 3
+
+# Session End
